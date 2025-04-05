5a08e3a8bed27868f744dd5a19f4ce4dec5cb851
elconercio <xcdrxcdr@gmail.com>
2025-04-05 18:14:02 -0500

    kabro

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://elconercio.github.io',
-  base:'/kabro',
+  base:'/kabro/',
   integrations: [
     tailwind(),
     svelte(),
