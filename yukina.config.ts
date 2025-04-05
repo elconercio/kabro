796059d758338f9cb51a0229472185d48fb4d45e
elconercio <xcdrxcdr@gmail.com>
2025-04-05 15:57:34 -0500

    kabro

--- yukina.config.ts
+++ yukina.config.ts
@@ -8,7 +8,7 @@ const YukinaConfig: Configuration = {
 
   description: "Demo Site",
 
-  site: "https://yukina-blog.vercel.app",
+  site: "https://elconercio.github.io",
 
   locale: "en", // set for website language and date format
 
